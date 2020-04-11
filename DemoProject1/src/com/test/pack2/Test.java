6a00e0f2013819b38326cda05f2a467408db0158
DEV2 <DEV2@gmail.com>
2020-04-11 18:51:53 +0530

    Dev1 modified Test.java

--- DemoProject1/src/com/test/pack2/Test.java
+++ DemoProject1/src/com/test/pack2/Test.java
@@ -1,6 +1,14 @@
 package com.test.pack2;
 
+import com.test.pack1.Utility;
+
 public class Test {
+	
+	public static void main(String[] args) {
+		
+		Utility utility = new Utility();
+		System.out.println(utility.getSysDate());
+	}
 	
 
 }
