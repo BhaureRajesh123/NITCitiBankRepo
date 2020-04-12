920a9cb9189baa9c4bd04e55e3031116560ac408
DEV2 <DEV2@gmail.com>
2020-04-12 17:48:46 +0530

    adding and modified

--- DemoProject1/src/com/test/pack1/Utility.java
+++ DemoProject1/src/com/test/pack1/Utility.java
@@ -3,18 +3,15 @@ package com.test.pack1;
 import java.util.Date;
 
 public class Utility {
-	
-	public Date getSysDate()
-	{
+
+	public Date getSysDate() {
 		return new Date();
 	}
-	
+
 	public void dummy()
 	{
-
-
 		System.out.println("===dummy() method=== 1234");
-
+		System.out.println("===dummy() method===abcd");      
 	}
 
 }
